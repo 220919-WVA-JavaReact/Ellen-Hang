d821668d4f6ed58d8955a0223d991e16d6897fad
EllenH479 <114024845+EllenH479@users.noreply.github.com>
2022-09-23 13:09:24 -0400

    csv-assignment

---
+++ Assignments/intro.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Ask the user for their name
+
+# echo Hello, who am I talking to?
+
+# read varname
+
+# echo It's nice to meet you $varname
+
+read -p 'Username: ' uservar
+read -sp 'Password: ' passvar
+echo
+echo Thank you $uservar we now have your login details
