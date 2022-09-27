29a925e56c66cd66fc749be12216993a3cd6bbe9
EllenH479 <114024845+EllenH479@users.noreply.github.com>
2022-09-27 14:57:23 -0400

    Countries

---
+++ Assignments/SQL-exercise/country.sql
@@ -0,0 +1,14 @@
+CREATE TABLE Country (
+ countryName varchar(30),
+ population int,
+ countryCity varchar(30),
+ currency varchar(30),
+ countryLanguage varchar(30)
+)
+
+INSERT INTO country VALUES ('Japan', '125000000', 'Tokyo', 'yen', 'Japanese');
+INSERT INTO country VALUES ('Gambia', '2417000', 'Banjul', 'dalasi', 'Wolof, Sarahole, Pulaar');
+insert into country values ('Ireland', '5100000', 'Dublin', 'euro, irish pound', 'Irish, English');
+insert into country values ('Portugal', '10310000', 'Lagos', 'euro', 'Portuguese, Mirandese');
+insert into country values ('Costa Rica', '5094000', 'San Jose', 'colon', 'Spanish');
+insert into country values ('Fiji', '896444', 'Suva', 'fijian', 'English, Fijian, Hindi');
